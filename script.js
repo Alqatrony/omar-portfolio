0c68c9aee6b5a1e5cc811930f6a8dbd24de7f75b
Alqatrony <59061290+Alqatrony@users.noreply.github.com>
2023-08-12 23:36:44 +0200

    edit header and cv.htlm to be responsive

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("button").addEventListener("click", function () {
-    window.location.hash = "work";
-});
+const toggle = document.getElementById('menu-toggle');
+const button2 = document.getElementById('BUTTON-2');
+toggle.onclick = function() {
+    toggle.classList.toggle('active');
+    document.getElementById('Group_12').classList.toggle('active');
+    setTimeout(function() {
+        button2.classList.toggle('active');
+    }, 300);
+}
+
+
